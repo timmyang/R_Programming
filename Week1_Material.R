3c5c1e61d519480ea434815b20bb267067c4a659
hyeonwooyang <tyang@wustl.edu>
2020-11-28 23:22:16 -0600

    11/28/2020 11:22 PM

--- Week1_Material.R
+++ Week1_Material.R
@@ -335,7 +335,7 @@ m
 
 #
 # Textual Data Formats
-# "dumping" and "dputing" preserves metadata(sacrificing some readability), so that antoher user doesn't have to specify it all over again.
+# "dumping" and "dputing" preserves metadata(sacrificing some readability), so that another user doesn't have to specify it all over again.
 # advantage: editable
 # disadvanage: not space-efficient
 
@@ -361,3 +361,410 @@ rm(x, y) # remove x and y
 source("data.R")
 y
 x
+
+#
+# Connections: Interfaces to the Outside World
+# "file" opens a connection to a file
+# "gzfile" opens a connection to a file compressed with gzip
+# "bzfile" opens a connection to a file compressed with bzip2
+# "url" opens a connection to a webpage
+
+str(file) # "str()" displays the internal structure of a R object
+# function (description = "", open = "", blocking = TRUE, encoding = getOption("encoding"), raw = FALSE, method = getOption("url.method", 
+# "default"))  
+
+# "description" is the name of the file
+# "open" is a code indicating
+# - "r" read only
+# - "w" writing (and initializina new file)
+# - "a" appending
+# - "rb", "wb", "ab" reading, writing, or appending in binary mode (Windows)
+
+con <- file("foo.txt", "r")
+data <- read.csv(con)
+close(con)
+
+# is the same as 
+data <- read.csv("foo.txt")
+
+
+connection <- url("http://www.jhsph.edu", "r")
+x <- readLines(connection)
+head(x)
+
+
+#
+# Subsetting: Basics
+# "[" always returns "an object" of the same class as original (vector -> vector) (list -> list)
+# can be used to select more than one element of an object (there is one exception)
+# "[[" extracts "elements" of a list or a dataframe
+# can only be used to extract a single element and the class of the returned object will not necessarily be a list or dataframe
+# "$" extracts "elements" of a list or dataframe "by name"
+  
+x <- c("a", "b", "c", "c", "d", "a")
+x[1]
+# [1] "a"
+x[1:4]                          # numeric index
+# [1] "a" "b" "c" "c"
+
+x[x > "a"]                      # logical index
+# [1] "b" "c" "c" "d"
+u <- x > "a"
+u
+# [1] FALSE  TRUE  TRUE  TRUE  TRUE FALSE
+x[u]
+# [1] "b" "c" "c" "d"
+
+#
+# Subsetting: Lists
+x <- list(foo = 1:4, bar = 0.6)
+x
+# $foo
+# [1] 1 2 3 4
+
+# $bar
+# [1] 0.6
+
+
+x[1]
+# $foo
+# [1] 1 2 3 4
+
+x[[1]]
+# [1] 1 2 3 4
+x[[2]]
+# [1] 0.6
+
+x$bar
+# [1] 0.6
+x[["bar"]]      # = x[[2]]
+# [1] 0.6
+x["bar"]        # = x[2]
+# $bar
+# [1] 0.6
+
+
+x <- list(foo = 1:4, bar = 0.6, baz = "hello")
+x
+# $foo
+# [1] 1 2 3 4
+
+# $bar
+# [1] 0.6
+
+# $baz
+# [1] "hello"
+
+x[c(1, 3)]
+# $foo
+# [1] 1 2 3 4
+
+# $baz
+# [1] "hello"
+
+name <- "foo"
+x[[name]] # = x[[1]]
+# [1] 1 2 3 4
+x$name          # element 'name' doesn't exist!
+# NULL
+x$foo
+# [1] 1 2 3 4           
+
+x <- list(a = list(10, 12, 14), b = c(3.14, 2.81))
+x
+# $a
+# $a[[1]]
+# [1] 10
+
+# $a[[2]]
+# [1] 12
+
+# $a[[3]]
+# [1] 14
+
+
+# $b
+# [1] 3.14 2.81
+
+x[[c(1, 3)]]
+# [1] 14
+x[[1]][[3]]
+# [1] 14
+x[[c(2, 1)]]
+# [1] 3.14
+
+#
+# Subsetting - Matrices
+x <- matrix(1:6, 2, 3)
+x
+#       [,1] [,2] [,3]
+# [1,]    1    3    5
+# [2,]    2    4    6
+
+x[1, 2]
+# [1] 3
+x[2, 1]
+# [1] 2
+
+x[1, ]
+# [1] 1 3 5
+x[, 2]
+# [1] 3 4
+
+# By default, when a single element of a matrix is retrieved, it is returned as a "vector" of length 1 rather than a 1 x 1 matrix
+# This behavior can be turned off by setting "drop = FALSE"
+x[1, 2]
+# [1] 3
+x[1, 2, drop = FALSE]
+#       [,1]
+# [1,]    3
+
+x[1, ]
+# [1] 1 3 5
+x[1, , drop = FALSE]
+#       [,1] [,2] [,3]
+# [1,]    1    3    5
+
+#
+# Subsetting - Partial Matching
+x <- list(aardvark = 1:5)
+x
+# $aardvark
+# [1] 1 2 3 4 5
+
+x$a
+# [1] 1 2 3 4 5
+x[["a"]]
+# NULL
+x [["a", exact = FALSE]]
+# [1] 1 2 3 4 5
+
+#
+# Subsetting - Removing Missing Values
+x <- c(1, 2, NA, 4, NA, 5)
+is.na(x)
+# [1] FALSE FALSE  TRUE FALSE  TRUE FALSE
+bad <- is.na(x)
+x[!bad]
+# [1] 1 2 4 5
+
+x <- c(1, 2, NA, 4, NA, 5)
+y <- c("a", "b", NA, "d", NA, NA)
+complete.cases(x, y)
+# [1]  TRUE  TRUE FALSE  TRUE FALSE FALSE
+good <- complete.cases(x, y)
+x[good]
+# [1] 1 2 4
+y[good]
+# [1] "a" "b" "d"
+
+airquality[1:6, ]
+#   Ozone Solar.R Wind Temp Month Day
+# 1    41     190  7.4   67     5   1
+# 2    36     118  8.0   72     5   2
+# 3    12     149 12.6   74     5   3
+# 4    18     313 11.5   62     5   4
+# 5    NA      NA 14.3   56     5   5
+# 6    28      NA 14.9   66     5   6
+complete.cases(airquality[1:6, ])
+# [1]  TRUE  TRUE  TRUE  TRUE FALSE FALSE
+good <- complete.cases(airquality)
+airquality[good, ][1:6, ]
+#   Ozone Solar.R Wind Temp Month Day
+# 1    41     190  7.4   67     5   1
+# 2    36     118  8.0   72     5   2
+# 3    12     149 12.6   74     5   3
+# 4    18     313 11.5   62     5   4
+# 7    23     299  8.6   65     5   7
+# 8    19      99 13.8   59     5   8
+
+#
+# Vectorized Operations
+x <- 1:4; y <- 6:9
+x
+# [1] 1 2 3 4
+y
+# [1] 6 7 8 9
+
+x + y
+# [1]  7  9 11 13
+x > 2
+# [1] FALSE FALSE  TRUE  TRUE
+x >= 2
+# [1] FALSE  TRUE  TRUE  TRUE
+y == 8
+# [1] FALSE FALSE  TRUE FALSE
+x * y
+# [1]  6 14 24 36
+x / y
+# [1] 0.1666667 0.2857143 0.3750000 0.4444444
+
+x <- matrix(1:4, 2, 2); y <- matrix(rep(10, 4), 2, 2)
+x
+#       [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+y
+#       [,1] [,2]
+# [1,]   10   10
+# [2,]   10   10
+
+x * y                   # element-wise multiplication
+#       [,1] [,2]
+# [1,]   10   30
+# [2,]   20   40
+x / y
+#       [,1] [,2]
+# [1,]  0.1  0.3
+# [2,]  0.2  0.4
+x %*% y                 # true matrix multiplication
+#       [,1] [,2]
+# [1,]   40   40
+# [2,]   60   60
+
+
+################################# Week 1 Quiz #################################
+# Question 1
+# The R language is a dialect of which of the following programming languages?
+# (Answer): S
+
+# Question 2
+# The definition of free software consists of four freedoms (freedoms 0 through 3). Which of the following is NOT one of the freedoms that are part of the definition? Select all that apply.
+# (Answer): all except the following 
+# Freedom 0: to run the program, for any purpose
+# Freedom 1: to study how the program works, and adapt it to your needs
+# Freedom 2: to redistribute copies so you can help your neighbor
+# Freedom 3: improve the program, and release your improvements to the public, so that the whole community benefits
+
+# Question 3
+# In R the following are all atomic data types EXCEPT: (Select all that apply)
+# (Answer):
+# (atomic data types): numeric, complex, integer, logical, character
+# (non-atomic data types): table, matrix, array, data frame, list
+
+# Question 4
+# If I execute the expression x <- 4 in R, what is the class of the object `x' as determined by the `class()' function?
+x <- 4
+class(x)
+# [1] "numeric"
+
+# Question 5
+# What is the class of the object defined by the expression x <- c(4, "a", TRUE)?
+x <- c(4, "a", TRUE)
+class(x)
+# [1] "character"
+
+# Question 6
+# If I have two vectors x <- c(1,3, 5) and y <- c(3, 2, 10), what is produced by the expression rbind(x, y)?
+x <- c(1, 3, 5); y <- c(3, 2, 10)
+rbind(x, y)
+#    [,1] [,2] [,3]
+# x    1    3    5
+# y    3    2   10
+
+# Question 7
+# A key property of vectors in R is that
+# (Answer): elements of a vector all must be of the same class
+
+# Question 8
+# Suppose I have a list defined as x <- list(2, "a", "b", TRUE). What does x[[2]] give me? Select all that apply
+x <- list(2, "a", "b", TRUE)
+x[[2]]
+# [1] 3
+
+# Question 9
+# Suppose I have a vector x <- 1:4 and a vector y <- 2. What is produced by the expression x + y?
+x <- 1:4; y <- 2
+x + y
+# [1] 3 4 5 6
+
+# Question 10
+# Suppose I have a vector x <- c(17, 14, 4, 5, 13, 12, 10) and I want to set all elements of this vector that are greater than 10 to be equal to 4. What R code achieves this? Select all that apply.
+x <- c(17, 14, 4, 5, 13, 12, 10)
+x[x > 10] <- 4
+# OR
+x[x >= 11] <- 4
+
+# Question 11
+#Use the Week 1 Quiz Data Set to answer questions 11-20.
+# In the dataset provided for this Quiz, what are the column names of the dataset?
+getwd()
+# [1] "/Users/hyeonwooyang/Desktop/Desktop/5_Coursera/1_R_Programming/git_project"
+setwd("~/Desktop/Desktop/5_Coursera/1_R_Programming/data")
+dir()
+
+# [1] "hw1_data.csv"
+table <- read.csv("hw1_data.csv")
+
+colnames(table)
+# [1] "Ozone"   "Solar.R" "Wind"    "Temp"    "Month"   "Day"  
+
+# Question 12
+# Extract the first 2 rows of the data frame and print them to the console. What does the output look like?
+table[1:2, ]
+head(table, 2)
+#    Ozone Solar.R Wind Temp Month Day
+# 1    41     190  7.4   67     5   1
+# 2    36     118  8.0   72     5   2
+
+# Question 13
+# How many observations (i.e. rows) are in this data frame?
+nrow(table)
+# [1] 153
+
+# Question 14
+# Extract the last 2 rows of the data frame and print them to the console. What does the output look like?
+tail(table, 2)
+#      Ozone Solar.R Wind Temp Month Day
+# 152    18     131  8.0   76     9  29
+# 153    20     223 11.5   68     9  30
+
+# Question 15
+# What is the value of Ozone in the 47th row?
+which(colnames(table) == "Ozone")
+match("Ozone", colnames(table))
+# [1] 1
+
+table[47, 1]
+# [1] 21
+
+# Question 16
+# How many missing values are in the Ozone column of this data frame?
+sum(is.na(table[, 1]))
+# [1] 37
+
+# Question 17
+# What is the mean of the Ozone column in this dataset? Exclude missing values (coded as NA) from this calculation.
+mean(table[, 1], na.rm = TRUE)
+# [1] 42.12931
+# OR
+x <- complete.cases(table[, 1])
+mean(table[x, 1])
+# [1] 42.12931
+# OR
+x <- is.na(table[, 1])
+mean(table[!x, 1])
+# [1] 42.12931
+
+# Question 18
+# Extract the subset of rows of the data frame where Ozone values are above 31 and Temp values are above 90. What is the mean of Solar.R in this subset?
+x <- complete.cases(table[, "Ozone"], table[, "Temp"])
+y <- table[x, ]
+View(y)
+
+z <- y[y$Ozone > 31 & y$Temp > 90, ]
+View(z)
+mean(z[, "Solar.R"])
+# [1] 212.8
+
+# Question 19
+# What is the mean of "Temp" when "Month" is equal to 6?
+x <- table[table$Month == 6, ]
+mean(x[, "Temp"])
+# [1] 79.1
+
+# Question 20
+# What was the maximum ozone value in the month of May (i.e. Month is equal to 5)?
+x <- table[table$Month == 5, ]
+max(x[, "Ozone"], na.rm = TRUE)
+# [1] 115
